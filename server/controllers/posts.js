782e623607b946f6362819ba4fd4911fd54305be
Pankrit Jindal <pankritjindal30@gmail.com>
2023-04-06 01:31:34 +1000

    posts route complete, backend complete

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -32,3 +32,55 @@ export const createPost = async (req,res) => {
         res.status(409).json({ message: err.message });
     }
 }
+
+/** READ */
+export const getFeedPosts = async (req, res) => {
+    // grab all the posts of everyone (something like a newsfeed for each)
+    // create feed of a user
+    try {
+        const post = await Post.find();
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(404).json({ message: err.message});
+    }
+}
+
+export const getUserPosts = async (req, res) => {
+    // get the user feed
+    try {
+        const { userId } = req.params;  // get the requested user
+        const post = await Post.find({ userId });   // grab all user posts
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(404).json({ message: err.message});
+    }
+}
+
+/** UPDATE */
+export const likePost = async (req, res) => {
+    try {
+        const { id } = req.params;  // grab the relevant postId (from the query string)
+        const { userId } = req.body;    // from req.body as the userId is sent to the body by the frontend this time. ie. comes from the body
+        const post = await Post.findById(id);
+        const isLiked = post.likes.get(userId); // check if the userId exists in likes. ie. if userId is there in likes, -> post is liked by that user
+
+        if (isLiked) {
+            // if the user has already liked the post, unlike it 
+            post.likes.delete(userId);
+        }
+        else {
+            // sets it if it doesn't exist
+            post.likes.set(userId, true);
+        }
+
+        const updatedPost = await Post.findByIdAndUpdate(
+            id,
+            { likes: post.likes },   // post.likes = list of likes we modified above. Set the likes list to the new updated list
+            { new: true }   // new object
+        );
+
+        res.status(200).json(updatedPost);  // updates the frontend once the like button is hit
+    } catch (err) {
+        res.status(404).json({ message: err.message});
+    }    
+};
