160da1f33804384e72250f7c65cb8508d3553bda
Pankrit Jindal <pankritjindal30@gmail.com>
2023-04-10 14:35:39 +1000

    add post widget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -2,6 +2,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidgets from "scenes/widgets/UserWidgets";
+import MyPostWidget from "scenes/widgets/MyPostWidget"
 
 const HomePage = () => {
     const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
@@ -16,15 +17,17 @@ const HomePage = () => {
                 display={isNonMobileScreens ? "flex" : "block"} // bigger screen: 3 components side by side, smaller screen: 3 components stacked on top of each other
                 gap="0.5rem"
                 justifyContent="space-between"
-            >
+                >
                 <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+{/* User Widget */}
                     <UserWidgets userId={_id} picturePath={picturePath} />
                 </Box>
                 <Box 
                     flexBasis={isNonMobileScreens ? "42%" : undefined}
                     mt={isNonMobileScreens ? undefined : "2rem"}
                 >
-                    {/* <Feed /> */}
+{/* Create Post Widget */}
+                    <MyPostWidget picturePath={picturePath} />
                 </Box>
                 {/* Friends list */}
                 {isNonMobileScreens && (<Box flexBasis="26%"></Box>)}
